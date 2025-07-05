283f2606c2754f0de22632595f4c5a646a9fbe51
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2025-07-06 03:09:09 +0500

    update shipment schema

--- src/models/shipmentSchema.model.js
+++ src/models/shipmentSchema.model.js
@@ -1,33 +1,75 @@
 import mongoose from 'mongoose'
 const shipmentSchema = new mongoose.Schema({
-    BiltyNo: {type: String,required: true},
-    InvoiceNo: {type: String,required: true},
-    SenderName: {type: String,required: true},
-    SenderAddressDetail: {type: String,required: true},
-    SenderMobile: {type: Number,required: true},
-    SenderCity: {type: String,required: true},
-    SenderOtherDetails: { type: String },
-    ReceiverName: {type: String,required: true},
-    ReceiverAddressDetail: {type: String,required: true},
-    ReceiverMobile: {type: Number,required: true},
-    ReceiverCity: {type: String,required: true},
-    ReceiverOtherDetail: {type: String},
-    NoOfPieces: {type: Number,required: true},
-    BranchName: {type: String,required: true},
-    DetailOfItems: { type: String },
-    // cargo
-    UnitRate: {type: Number,required: true},
-    TotalWeight: {type: Number,required: true},
-    TotalAmount: {type: Number,required: true},
-    Customs: {type: Number},
-    Packaging: {type: Number},
-    Shipping: {type: Number},
-    Clearance: {type: Number},
-    VAT: {type: Number},
-    VAT_Value: {type: Number},
-    OtherCharges: {type: Number},
-    TotalInvoiceAmount: {type: Number},
-    PublishDate: { type: Date, default: Date.now, }
+    SenderName: { type: String, required: true },
+    SenderMobile: { type: String, required: true },
+    SenderIdNumber: { type: String, required: true },
+    SenderAddress: { type: String, required: true },
+    SenderArea: { type: String,required: true },
+
+    ReceiverName: { type: String, required: true },
+    ReceiverMobile1: { type: String, required: true },
+    ReceiverMobile2: { type: String ,required: true},
+    ReceiverAddress: { type: String, required: true },
+
+    ItemDetails: { type: String },
+    OtherDetails: { type: String },
+
+    NoOfPieces: { type: Number, required: true },
+    Branch: { type: String, required: true },
+    BookingDate: { type: String,required: true }, // format: YYYY-MM-DD
+
+    Charges: {
+        FreightCharges: {
+            enabled: { type: Boolean, default: false },
+            unitRate: { type: Number, default: 0 },
+            qty: { type: Number, default: 0 },
+            total: { type: Number, default: 0 },
+        },
+        Insurance: {
+            enabled: { type: Boolean, default: false },
+            unitRate: { type: Number, default: 0 },
+            qty: { type: Number, default: 0 },
+            total: { type: Number, default: 0 },
+          },
+          Packing: {
+            enabled: { type: Boolean, default: false },
+            unitRate: { type: Number, default: 0 },
+            qty: { type: Number, default: 0 },
+            total: { type: Number, default: 0 },
+        },
+        Customs: {
+            enabled: { type: Boolean, default: false },
+            unitRate: { type: Number, default: 0 },
+            qty: { type: Number, default: 0 },
+            total: { type: Number, default: 0 },
+          },
+          Clearance: {
+            enabled: { type: Boolean, default: false },
+            unitRate: { type: Number, default: 0 },
+            qty: { type: Number, default: 0 },
+            total: { type: Number, default: 0 },
+          },
+          OtherCharges: {
+            enabled: { type: Boolean, default: false },
+            unitRate: { type: Number, default: 0 },
+            qty: { type: Number, default: 0 },
+            total: { type: Number, default: 0 },
+          }
+    },
+    Discount: {
+        enabled: { type: Boolean, default: false },
+        discount: { type: Number, default: 0 }
+      },
+    SubTotal: { type: Number },
+    Vat: { type: Number },
+    VatTotal: { type: Number },
+    InvoiceTotal: { type: Number },
+    
+    AmountInWords: { type: String },
+    
+    BiltyNo: { type: String },
+    InvoiceNo: { type: String },
+    
 }, {
     timestamps:true
 }) 
