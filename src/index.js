0825d4357292cd59791307b71ce89422c4997462
Muhammad Adnan <adnan892009@gmail.com>
2025-10-22 18:06:30 +0500

    file update serverless

--- src/index.js
+++ src/index.js
@@ -10,17 +10,14 @@ import CountryRouter from '../src/routers/country.router.js'
 import ContainerNumberRouter from '../src/routers/container_number.router.js'
 import 'dotenv/config'
 import cors from 'cors'
+//import { use } from 'react'
 const app = express()
 app.use(express.json())
 app.use(cors())
 app.get('/', (req,res) => {
     res.send('Server is up and running!!!!')
 })
-
-// console.log(process.env.MONGO_URI);
-mongoose.connect(process.env.MONGO_URI).then(() => {
-    console.log('db connected succesfully')
-    app.use('/api',BookingRouter)
+ app.use('/api',BookingRouter)
     app.use('/api',ContainerRouter)
     app.use('/api', TrackingRouter)
     app.use('/api', WhatsappRouter)
@@ -29,13 +26,48 @@ mongoose.connect(process.env.MONGO_URI).then(() => {
      app.use('/api',CountryRouter)
      app.use('/api',ContainerNumberRouter)
     app.use('/uploads', express.static('uploads'));
-    const port = process.env.PORT || 5000
-    app.listen(port, () => {
-        console.log(`Server is running on port ${5000}`);
-        
-    })
+// console.log(process.env.MONGO_URI);
+// mongoose.connect(process.env.MONGO_URI).then(() => {
+//     console.log('db connected succesfully')
+   
     
-}).catch((e) => {
-        console.log("err=>",e);
+//     const port = process.env.PORT || 5000
+//     app.listen(port, () => {
+//         console.log(`Server is running on port ${5000}`);
         
-    });
+//     })
+    
+// }).catch((e) => {
+//         console.log("err=>",e);
+        
+//     });
+
+
+let isConnected = false;
+
+async function connectDB() {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+
+    isConnected = true;
+    console.log('DB Connected');
+  } catch (error) {
+   console.log("DB Connection Error:", error);
+    console.error("DB Connection Error:", error);
+  }
+}
+
+// Middleware to ensure DB connection
+app.use(async (req, res, next) => {
+  if (!isConnected) {
+    await connectDB();
+  }
+  next();
+});
+connectDB()
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+   
+});
